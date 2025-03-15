68f95f999070cb1c8e8eddca6648fd9f1aec1e7c
muhammad nur akmal <muhammad_nur_akmal_zubir@cpf.gov.sg>
2025-03-15 12:26:22 +0800

    TEST

--- workers/api-entrypoint/src/endpoints/embed.ts
+++ workers/api-entrypoint/src/endpoints/embed.ts
@@ -54,7 +54,7 @@ export class Embed extends OpenAPIRoute {
   async handle(c: Context) {
     // Get validated data
     const data = await this.getValidatedData<typeof this.schema>();
-    logger.info(c.env.MY_SECRET_NAME_KEY);
+    logger.info(c.env.ENVIRONMENT);
 
     // Retrieve the validated text
     const { text } = data.body;
